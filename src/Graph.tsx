58eebd9dd53ccd1121d57bbb0c5346b416fee97c
arnabsubedi22 <alexgamer1819@gmail.com>
2022-10-12 19:43:28 +0100

    task2

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement{
   load: (table: Table) => void,
 }
 
@@ -30,9 +30,10 @@ class Graph extends Component<IProps, {}> {
     return React.createElement('perspective-viewer');
   }
 
-  componentDidMount() {
+    componentDidMount() {
+      console.log('rendering');
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+      const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
@@ -41,16 +42,25 @@ class Graph extends Component<IProps, {}> {
       timestamp: 'date',
     };
 
-    if (window.perspective && window.perspective.worker()) {
+    if (window.perspective ) {
       this.table = window.perspective.worker().table(schema);
     }
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
-
+          console.log('change table');
       // Add more Perspective configurations here.
-      elem.load(this.table);
-    }
+          elem.load(this.table);
+          elem.setAttribute('view', 'y_line');
+          elem.setAttribute('column-pivots', '["stock"]');
+          elem.setAttribute('row-pivots', '["timestamp"]');
+          elem.setAttribute('columns', '["top_ask_price"]');
+          elem.setAttribute('aggregates', '{"stock":"distinct count","top_ask_price":"avg","top_bid_price":"avg","timestamp":"distinct count"}');
+      }
   }
+        
+        
+    
+  
 
   componentDidUpdate() {
     // Everytime the data props is updated, insert the data into Perspective table
